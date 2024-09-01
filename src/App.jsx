da1be1e49192f4a53b9a9038a0a139aaca8633fc
LiliiaVersta <versta270695@gmail.com>
2024-09-01 13:17:31 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,41 +10,27 @@ export const App = () => {
   let positive = Math.round((feedback.good / totalFeedback) * 100);
 
   const updateFeedback = (feedbackType) => {
-    let state = { ...feedback };
-    state[feedbackType] = state[feedbackType] + 1;
-    setToLocal(feedbackType, state[feedbackType]);
-    setFeedback({ ...state });
-  };
-  let getFromLocal = (key) => {
-    let val = localStorage.getItem(key);
-    if (!val) {
-      localStorage.setItem(key, 0);
-    }
-    let keyVal = +val || 0;
-    return keyVal;
-  };
-
-  let setToLocal = (key, val) => {
-    localStorage.setItem(key, val);
+    const state = { ...feedback, [feedbackType]: feedback[feedbackType] + 1 }
+  
+    setFeedback(state)
+    localStorage.setItem('feedbacks', JSON.stringify(state))
   };
 
   useEffect(() => {
-    let localGood = getFromLocal("good");
-    let localNeutral = getFromLocal("neutral");
-    let localBad = getFromLocal("bad");
-    let localState = {
-      good: localGood,
-      neutral: localNeutral,
-      bad: localBad,
-    };
-    setFeedback({ ...localState });
+    let localState = JSON.parse(localStorage.getItem("feedbacks"));
+    let computedState = localState || { good: 0, neutral: 0, bad: 0 };
+    setFeedback({ ...computedState });
   }, []);
-  const handleResetClick = () => {
-    setToLocal("good", 0);
-    setToLocal("neutral", 0);
-    setToLocal("bad", 0);
+
+ const handleResetClick = () => {
+    localStorage.setItem(
+      "feedbacks",
+      JSON.stringify({ good: 0, neutral: 0, bad: 0 })
+    );
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
+
+
   let feedbackOrNotification = totalFeedback ? (
     <Feedback data={feedback} positive={positive} total={totalFeedback} />
   ) : (
